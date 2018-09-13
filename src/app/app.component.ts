d5dc055a059b9aa81af94615a03cca94aa715fac
PiLMee GaTes <pilmee@MacBook-Pro-de-PiLMee.local>
2018-09-12 23:31:46 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sunat-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sunat';
+}
